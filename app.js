98efe76ac50310fad9344602785d091b2176a368
Anand Panda <anand612p@gmail.com>
2023-12-23 18:14:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const parent = React.createElement('div', {id: 'parent'}, [
+    React.createElement('div', {id: 'child1'}, [
+        React.createElement('h1', {}, "h1 tag"),
+        React.createElement('h2', {}, "h2 tag"),
+    ]),
+    React.createElement('div', {id: 'child2'}, [
+        React.createElement('h1', {}, "h1 tag"),
+        React.createElement('h2', {}, "h2 tag"),
+    ])
+]);
+
+root.render(parent);
