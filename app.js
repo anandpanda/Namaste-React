8f3bab97e7460401abdc17e3064a640409cd071e
Anand Panda <anand612p@gmail.com>
2023-12-24 22:53:37 +0530

    added

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const parent = React.createElement('div', {id: 'parent'}, [
-    React.createElement('div', {id: 'child1'}, [
-        React.createElement('h1', {}, "h1 tag"),
-        React.createElement('h2', {}, "h2 tag"),
-    ]),
-    React.createElement('div', {id: 'child2'}, [
-        React.createElement('h1', {}, "h1 tag"),
-        React.createElement('h2', {}, "h2 tag"),
-    ])
-]);
+const title = (
+    <h1 className="title">React Element</h1>
+);
+const Title = () => (
+    <h1 className="title">React component</h1>
+);
+const Heading = () => (
+    <div id="container">
+        {title}
+        <Title/> {/* component coposition */}
+        <h1 className="heading">React component</h1>
+    </div>
+);
 
-root.render(parent);
+root.render(<Heading/>);
